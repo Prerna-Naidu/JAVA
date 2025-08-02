ca7a3a365aeee94e486a52f6aa7fd407efb33721
Prerna-Naidu <prerna.naidu11@gmail.com>
2025-08-03 00:03:58 +0530

    Update BRReadLines.java

--- BRReadLines.java
+++ BRReadLines.java
@@ -6,7 +6,6 @@ BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 String str;
 System.out.println("Enter lines of text.");
 System.out.println("Enter 'stop' to quit.");
-
 do{
 str= br.readLine();
 System.out.println(str);
