63a0d1e1b6d7e04b05875f20fbdfa248d30c5950
Prerna-Naidu <prerna.naidu11@gmail.com>
2025-09-03 19:11:02 +0530

    Added package

---
+++ Package.java
@@ -0,0 +1,26 @@
+class Balance{
+    String name;
+    double bal;
+    Balance(String n,double b){
+        name=n;
+        bal=b;
+    }
+    void show(){
+        if(bal>0){
+            System.out.println("-->");
+            System.out.println(name+":$" +bal);
+        }
+    }
+}
+class Package{
+    public static void main(String[] args) {
+        Balance current[]=new Balance[3];
+        current[0]=new Balance("Sanjib",123.23);
+        current[1]=new Balance("Chandan",157.02);
+        current[2]=new Balance("Palak",-12.33);
+
+        for(int i=0;i<3;i++){
+            current[i].show();
+        }
+    }
+}
