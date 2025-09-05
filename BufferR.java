8d7ac59ab4a62af611f76a28dbbd9bb3bb2b7d35
Prerna-Naidu <prerna.naidu11@gmail.com>
2025-09-05 21:49:33 +0530

    Added Buffer Readers

---
+++ BufferR.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.io.InputStreamReader;
+class BufferR{
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        int x,y,z;
+        try{
+            System.out.println("Enter 1st number:");
+            str=br.readLine();
+            x=Integer.parseInt(str);
+            System.out.println("Enter 2nd number:");
+            str=br.readLine();
+            y=Integer.parseInt(str);
+            z=x+y;
+            System.out.println("Addition Result is: " +z);
+        }
+        catch(IOException e){
+            System.out.println("Invalid Number. Try Again !!!!!");
+        }
+    }
+}
